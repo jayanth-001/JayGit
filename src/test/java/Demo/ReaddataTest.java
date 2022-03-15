72b6f34a97c02fd198a1919e44e3caa6eecc05fb
Lenovo <Lenovo@DESKTOP-01H70DH>
2022-03-15 15:58:06 +0530

    new commit

---
+++ src/test/java/Demo/ReaddataTest.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class ReaddataTest {
+
+	@Test
+	public void read()
+	{
+		String BROSWER = System.getProperty("browser");
+		System.out.println(BROSWER);
+		System.out.println("done");
+		
+	}
+	
+	
+}
