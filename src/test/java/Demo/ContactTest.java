0b218367d3d8f4f6ce73c53b3eb878625d041fcf
Lenovo <Lenovo@DESKTOP-01H70DH>
2022-03-15 13:22:22 +0530

    Sample first commit

---
+++ src/test/java/Demo/ContactTest.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContTest()
+	{
+		System.out.println("Contact test pass");
+	}
+}
---
+++ src/test/java/orgTest/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package orgTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Org test passed");
+	}
+	 
+	/*@Test
+	public void createContTest()
+	{
+		System.out.println("Test-2 pass");
+	}*/
+}
